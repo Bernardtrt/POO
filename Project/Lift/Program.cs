582ceedaae1f5b0992d33b4775300405dfbc8443
bernardtrt <bernarddelatortue@gmail.com>
2026-02-15 17:14:18 +0000

    pushh

--- Project/Lift/Program.cs
+++ Project/Lift/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System;
-
+using System.Collections.Generic;
 class Programm
 {
     static void Main()
     {
-        Console.WriteLine("hello world !")
+        Console.WriteLine("hello world !");
+        var monEntrainement = new Workout{Name = "Push", Date = DateTime.Now};
+
+        var dc = new Exercice { Name = "Développé couché" };
+        dc.sets.Add(new Set { Reps = 10, Weight = 80 });
+        dc.sets.Add(new Set { Reps = 8, Weight = 85 });
+
+        monEntrainement.Exercices.Add(dc);
+
+        Console.WriteLine($"Séance : {monEntrainement.Name}");
+        Console.WriteLine($"Volume Total : {monEntrainement.TotalVolume()} kg");
     }
+
+}
+
+
+class User
+{
+    public string username {get; set;} = string.Empty;
+    public List<Workout> History {get; set;} = new();
+}
+
+class Workout{
+    public string Name {get; set;} = "New workout";
+    public DateTime Date { get; set; }
+    public List<Exercice> Exercices {get; set;} = new();
+
+
+    public double TotalVolume()
+    {
+        double total = 0;
+        foreach(var exo in Exercices){
+            foreach (var s in exo.sets){
+                total += s.Weight * s.Reps;
+            }
+        }
+        return total;
+
+    }
+
+}
+
+class Exercice{
+    public string Name {get; set;} = string.Empty;
+    public List<Set> sets {get; set;} = new();
+}
+
+class Set{
+    public int Reps {get; set;}
+    public double Weight {get; set;}
+
 }
--- Project/Lift/obj/Debug/net8.0/Lift.AssemblyInfo.cs
+++ Project/Lift/obj/Debug/net8.0/Lift.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lift")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0958620488bfa90d0c788d41ed430ccfeab4db0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lift")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lift")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
